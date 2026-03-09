2327510b5b1d62109f0e58c1f3dd4064391ab349
five <co22rkn@protonmail.com>
2026-03-09 15:25:03 +0100

    new repo: 'go to repo' instead of 'navigate to repo'

--- src/lib/components/git/RepoProgressStep.svelte
+++ src/lib/components/git/RepoProgressStep.svelte
@@ -166,7 +166,7 @@
           onclick={() => onNavigateToRepo(createdRepoResult)}
           class="px-4 py-2 text-sm font-medium text-white bg-green-600 hover:bg-green-700 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition-colors"
         >
-          Navigate to repo
+          Go to repo
         </button>
       {/if}
 
